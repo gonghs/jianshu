41fc53cf58fe4dc59e8cd8d6502afc3299e05455
maple <944123439@qq.com>
2020-05-23 19:12:12 +0800

    [新增功能](1.0.0): shiro-permissions-aop 测试代码

--- shiro-permissions-aop/src/main/java/com/maple/GlobalExceptionHandler.java
+++ shiro-permissions-aop/src/main/java/com/maple/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.maple;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,10 +17,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(value = AuthorizationException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String AuthorizationException(AuthorizationException throwable) {
+        log.error("发生了异常: ", throwable);
+        return "权限注解生效";
+    }
+
     @ExceptionHandler(value = Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String defaultErrorHandler(Throwable throwable) {
         log.error("发生了异常: ", throwable);
-        return "error";
+        return "权限注解生效";
     }
 }
